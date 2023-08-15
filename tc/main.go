5f09253a406b841836c1791bcd3b73e8cffad692
spoock <me@spoock.com>
2023-08-16 00:05:44 +0800

    [+]update tc example

--- tc/main.go
+++ tc/main.go
@@ -1,1 +1,27 @@
-package tc
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	manager "github.com/gojue/ebpfmanager"
+)
+
+//go:embed ebpf/bin/probe.o
+var _bytecode []byte
+
+func main() {
+	m := &manager.Manager{
+		Probes: []*manager.Probe{
+			{
+				Section:      "sockops",
+				EbpfFuncName: "bpf_sockops",
+			},
+		},
+	}
+	err := m.Init(bytes.NewReader(_bytecode))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
